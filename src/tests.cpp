5d0bd19440e559d33feddaf7253fb08ee826af68
schlumphi <j.zarzycki777@gmail.com>
2026-02-22 21:29:30 +0100

    test: constructing Student object test case

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,9 +1,27 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+TEST(Student, ConstructorWithValidDataCreatesValidObject) {
+    Student abacki{
+        "Adam",
+        "Abacki",
+        Address{
+            "ul. Warszawska",
+            "42",
+            "2",
+            PostalCode{"01-234"},
+            "Warszawa"},
+        42ULL,
+        Pesel{"99123101234"},
+        Gender::Male};
+
+    EXPECT_STREQ(abacki.first_name(), "Adam");
+    EXPECT_STREQ(abacki.last_name(), "Abacki");
+    EXPECT_STREQ(abacki.address().street(), "ul. Warszawska");
+    EXPECT_STREQ(abacki.address().house_number(), "42");
+    EXPECT_STREQ(abacki.address().apartment_number(), "2");
+    EXPECT_STREQ(abacki.postal_code().value(), "01-234");
+    EXPECT_STREQ(abacki.city(), "Warszawa");
+    EXPECT_EQ(abacki.index_num(), 42ULL);
+    EXPECT_STREQ(abacki.pesel().value(), "99123101234");
+    EXPECT_EQ(abacki.gender(), Gender::Male);
 }
