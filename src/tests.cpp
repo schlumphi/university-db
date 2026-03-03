d11063bcf2db037550f88cd3a3c7022f4cb88d22
schlumphi <j.zarzycki777@gmail.com>
2026-03-03 21:50:45 +0100

    test: fix student members access

--- src/tests.cpp
+++ src/tests.cpp
@@ -16,14 +16,14 @@ TEST(Student, ConstructorWithValidDataCreatesValidObject) {
         Pesel{"99123101234"},
         Gender::Male};
 
-    EXPECT_STREQ(abacki.first_name(), "Adam");
-    EXPECT_STREQ(abacki.last_name(), "Abacki");
-    EXPECT_STREQ(abacki.address().street(), "ul. Warszawska 42");
-    EXPECT_STREQ(abacki.address().apartment(), "2");
-    EXPECT_STREQ(abacki.postal_code().value(), "01-234");
-    EXPECT_STREQ(abacki.city(), "Warszawa");
+    EXPECT_EQ(abacki.first_name(), "Adam");
+    EXPECT_EQ(abacki.last_name(), "Abacki");
+    EXPECT_EQ(abacki.address().street(), "ul. Warszawska 42");
+    EXPECT_EQ(abacki.address().apartment(), "2");
+    EXPECT_EQ(abacki.address().postal_code().value(), "01-234");
+    EXPECT_EQ(abacki.address().city(), "Warszawa");
     EXPECT_EQ(abacki.index_num(), 42ULL);
-    EXPECT_STREQ(abacki.pesel().value(), "99123101234");
+    EXPECT_EQ(abacki.pesel().value(), "99123101234");
     EXPECT_EQ(abacki.gender(), Gender::Male);
 }
 
