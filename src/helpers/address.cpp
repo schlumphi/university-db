daeb710fef6e796aae541c59b7df0d7b5ef8a32f
schlumphi <j.zarzycki777@gmail.com>
2026-03-03 19:27:29 +0100

    feat: Address implementation

--- src/helpers/address.cpp
+++ src/helpers/address.cpp
@@ -1,1 +1,9 @@
 #include "address.hpp"
+
+Address::Address(
+    const std::string& street,
+    const std::string& apartment,
+    const PostalCode& postal_code,
+    const std::string& city)
+    : m_street(street), m_apartment(apartment), m_postal_code(postal_code), m_city(city) {
+}
--- src/helpers/address.hpp
+++ src/helpers/address.hpp
@@ -1,1 +1,27 @@
 #pragma once
+#include <string>
+#include "postal_code.hpp"
+
+class Address {
+public:
+    Address(
+        const std::string& street,
+        const std::string& apartment,
+        const PostalCode& postal_code,
+        const std::string& city);
+
+    auto street() const noexcept -> const std::string& { return m_street; }
+    auto apartment() const noexcept -> const std::string& { return m_apartment; }
+    auto postal_code() const noexcept -> const PostalCode& { return m_postal_code; }
+    auto city() const noexcept -> const std::string& { return m_city; }
+
+private:
+    const std::string m_street;
+    const std::string m_apartment;
+    const PostalCode m_postal_code;
+    const std::string m_city;
+
+    auto validate_street(const std::string& street) const noexcept -> bool;
+    auto validate_apartment(const std::string& apartment) const noexcept -> bool;
+    auto validate_city(const std::string& city) const noexcept -> bool;
+};
