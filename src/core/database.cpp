37d19a6b78f6b191027e2f0114f04f6a5951495a
Jakub Zarzycki <j.zarzycki777@gmail.com>
2026-03-06 14:44:57 +0100

    feat: adding new students

--- src/core/database.cpp
+++ src/core/database.cpp
@@ -1,1 +1,10 @@
 #include "database.hpp"
+#include <algorithm>
+
+auto Database::add(const Student& student) -> bool {
+    if (std::find(m_state.begin(), m_state.end(), student) != m_state.end()) {
+        return false;
+    }
+    m_state.emplace_back(student);
+    return true;
+}
--- src/core/database.hpp
+++ src/core/database.hpp
@@ -1,1 +1,11 @@
 #pragma once
+#include <list>
+#include "student.hpp"
+
+class Database {
+public:
+    auto add(const Student& student) -> bool;
+
+private:
+    std::list<Student> m_state;
+};
