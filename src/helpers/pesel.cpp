4056668ef932087c165c2990f472c3d812256468
Jakub Zarzycki <j.zarzycki777@gmail.com>
2026-03-08 20:10:15 +0100

    feat: pesel error code parser

--- src/helpers/pesel.cpp
+++ src/helpers/pesel.cpp
@@ -8,3 +8,16 @@ Pesel::Pesel(const std::string& number) : m_number(number) {
         throw std::runtime_error("Pesel::Pesel: expected 'number' to be 11 digits length.");
     }
 }
+
+auto validate_number(const std::string& number) noexcept -> std::optional<Pesel::ErrorCode> {
+    return std::nullopt;
+}
+
+auto parse_pesel_error_code(Pesel::ErrorCode error) -> std::string_view {
+    switch (error) {
+    case Pesel::ErrorCode::InvalidPeselLength:
+        return "expected 'number' to be 11 digits length";
+    default:
+        return "Ok";
+    }
+}
--- src/helpers/pesel.hpp
+++ src/helpers/pesel.hpp
@@ -1,12 +1,20 @@
 #pragma once
+#include <optional>
 #include <string>
+#include <string_view>
 
 class Pesel {
 public:
+    enum class ErrorCode {
+        InvalidPeselLength
+    };
     Pesel(const std::string& number);
 
     auto value() const noexcept -> const std::string& { return m_number; }
 
 private:
+    static auto validate_number(const std::string& number) noexcept -> std::optional<ErrorCode>;
     const std::string m_number;
 };
+
+auto parse_pesel_error_code(Pesel::ErrorCode error) -> std::string_view;
