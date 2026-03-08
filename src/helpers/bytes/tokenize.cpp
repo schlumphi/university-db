c93a65fe7805dc26d47eba5af0099ab4a3ef7049
Jakub Zarzycki <j.zarzycki777@gmail.com>
2026-03-08 18:50:19 +0100

    feat: tokenizator fun

---
+++ src/helpers/bytes/tokenize.cpp
@@ -0,0 +1,19 @@
+#include "tokenize.hpp"
+
+auto tokenize(const std::string& data, const char delim) -> std::vector<std::string> {
+    std::vector<std::string> tokens;
+    std::string buffer;
+    for (auto c : data) {
+        if (c == delim && !buffer.empty()) {
+            tokens.push_back(buffer);
+            buffer.clear();
+        } else {
+            buffer.push_back(c);
+        }
+    }
+    if (!buffer.empty()) {
+        tokens.push_back(buffer);
+    }
+
+    return tokens;
+}
---
+++ src/helpers/bytes/tokenize.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+auto tokenize(const std::string& data, const char delim) -> std::vector<std::string>;
