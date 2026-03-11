cd3394a7edcc3e60bd83e308543263095de8b235
schlumphi <j.zarzycki777@gmail.com>
2026-03-11 19:47:40 +0100

    feat: pesel format validation

--- src/helpers/pesel.cpp
+++ src/helpers/pesel.cpp
@@ -4,18 +4,21 @@
 #include <iostream>
 
 Pesel::Pesel(const std::string& number) : m_number(number) {
-    if (number.length() != 11 || std::any_of(number.begin(), number.end(), [](char c) { return !std::isdigit(c); })) {
-        throw std::runtime_error("Pesel::Pesel: expected 'number' to be 11 digits length.");
+    if (auto error = validate_format(number)) {
+        throw std::invalid_argument(std::string(parse_pesel_error_code(*error)));
     }
 }
 
-auto validate_number(const std::string& number) noexcept -> std::optional<Pesel::ErrorCode> {
+auto Pesel::validate_format(const std::string& number) noexcept -> std::optional<Pesel::ErrorCode> {
+    if (number.length() != 11 || std::any_of(number.begin(), number.end(), [](char c) { return !std::isdigit(c); })) {
+        return Pesel::ErrorCode::InvalidPeselFormat;
+    }
     return std::nullopt;
 }
 
 auto parse_pesel_error_code(Pesel::ErrorCode error) -> std::string_view {
     switch (error) {
-    case Pesel::ErrorCode::InvalidPeselLength:
+    case Pesel::ErrorCode::InvalidPeselFormat:
         return "expected 'number' to be 11 digits length";
     default:
         return "Ok";
--- src/helpers/pesel.hpp
+++ src/helpers/pesel.hpp
@@ -2,18 +2,22 @@
 #include <optional>
 #include <string>
 #include <string_view>
+#include "gender.hpp"
 
 class Pesel {
 public:
     enum class ErrorCode {
-        InvalidPeselLength
+        InvalidPeselFormat
     };
     Pesel(const std::string& number);
 
     auto value() const noexcept -> const std::string& { return m_number; }
+    auto gender() const noexcept -> const Gender;
 
 private:
-    static auto validate_number(const std::string& number) noexcept -> std::optional<ErrorCode>;
+    static auto validate_format(const std::string& number) noexcept -> std::optional<ErrorCode>;
+    static auto validate_correctness(const std::string& number) noexcept -> std::optional<ErrorCode>;
+
     const std::string m_number;
 };
 
