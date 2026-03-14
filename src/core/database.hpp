691d204c73f5c108b15d944f36833ba1f4727b12
Jakub Zarzycki <j.zarzycki777@gmail.com>
2026-03-14 17:51:34 +0100

    feat: display db

--- src/core/database.hpp
+++ src/core/database.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include <array>
 #include <list>
 #include <optional>
+#include <string_view>
 #include "student.hpp"
 
 class Database {
@@ -8,10 +10,17 @@ public:
     enum class ErrorCode {
         StudentAlreadyExistsInDb
     };
-    auto add(const Student& student) -> std::optional<ErrorCode>;
+
+    auto add(const Student& student) noexcept -> std::optional<ErrorCode>;
+    auto display(const char sep = '|') const noexcept -> std::string;
+
+    static auto parse_student(const Student& student) noexcept -> std::array<std::string, 9>;
+
+    static constexpr std::array<std::string_view, 9> columns{
+        "first_name", "last_name", "street", "apartment", "postal_code", "city", "index_num", "pesel", "gender"};
 
 private:
     std::list<Student> m_state;
 };
 
-auto parse_database_error_code(Database::ErrorCode error) -> std::string_view;
+auto parse_database_error_code(Database::ErrorCode error) noexcept -> std::string_view;
