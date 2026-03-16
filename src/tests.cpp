5fb50ae90a5088a98ebfb87c3b6882fff67b6ea2
schlumphi <j.zarzycki777@gmail.com>
2026-03-16 17:05:49 +0100

    test: sort by pesel ut

--- src/tests.cpp
+++ src/tests.cpp
@@ -2,6 +2,92 @@
 #include "core/database.hpp"
 #include "core/student.hpp"
 
+TEST(Database, SortByPesel) {
+    Student abacki{
+        "Adam",
+        "Abacki",
+        Address{
+            "Warszawska 42",
+            "2",
+            PostalCode{"01-234"},
+            "Warszawa"},
+        Pesel{"55030101193"},
+        Gender::Male};
+
+    Student babacki{
+        "Bdam",
+        "Babacki",
+        Address{
+            "Warszawska 43",
+            "3",
+            PostalCode{"01-234"},
+            "Warszawa"},
+        Pesel{"55030101230"},
+        Gender::Male};
+
+    Student cabacki{
+        "Cdam",
+        "Cabacki",
+        Address{
+            "Warszawska 44",
+            "4",
+            PostalCode{"01-234"},
+            "Warszawa"},
+        Pesel{"83050594899"},
+        Gender::Male};
+
+    Student dabacka{
+        "Ddama",
+        "Dabacka",
+        Address{
+            "Warszawska 45",
+            "5",
+            PostalCode{"01-234"},
+            "Warszawa"},
+        Pesel{"03281592527"},
+        Gender::Female};
+
+    Student fabacki{
+        "Fdam",
+        "Fabacki",
+        Address{
+            "Warszawska 46",
+            "6",
+            PostalCode{"01-234"},
+            "Warszawa"},
+        Pesel{"52030218632"},
+        Gender::Male};
+
+    Database db;
+    db.add(abacki);
+    db.add(babacki);
+    db.add(cabacki);
+    db.add(dabacka);
+    db.add(fabacki);
+
+    const std::string ref_db_display_sorted_ascending =
+        "first_name|last_name|street|apartment|postal_code|city|index_num|pesel|gender\n"
+        "Ddama|Dabacka|Warszawska 45|5|01-234|Warszawa|4|03281592527|female\n"
+        "Cdam|Cabacki|Warszawska 44|4|01-234|Warszawa|3|83050594899|male\n"
+        "Bdam|Babacki|Warszawska 43|3|01-234|Warszawa|2|55030101230|male\n"
+        "Adam|Abacki|Warszawska 42|2|01-234|Warszawa|1|55030101193|male\n"
+        "Fdam|Fabacki|Warszawska 46|6|01-234|Warszawa|5|52030218632|male\n";
+
+    db.sort_by_pesel(Database::SortOrder::Ascending);
+    EXPECT_EQ(db.display(), ref_db_display_sorted_ascending);
+
+    const std::string ref_db_display_sorted_descending =
+        "first_name|last_name|street|apartment|postal_code|city|index_num|pesel|gender\n"
+        "Fdam|Fabacki|Warszawska 46|6|01-234|Warszawa|5|52030218632|male\n"
+        "Bdam|Babacki|Warszawska 43|3|01-234|Warszawa|2|55030101230|male\n"
+        "Adam|Abacki|Warszawska 42|2|01-234|Warszawa|1|55030101193|male\n"
+        "Cdam|Cabacki|Warszawska 44|4|01-234|Warszawa|3|83050594899|male\n"
+        "Ddama|Dabacka|Warszawska 45|5|01-234|Warszawa|4|03281592527|female\n";
+
+    db.sort_by_pesel(Database::SortOrder::Descending);
+    EXPECT_EQ(db.display(), ref_db_display_sorted_descending);
+}
+
 TEST(Database, SearchByLastName) {
     Student abacki{
         "Adam",
@@ -83,7 +169,9 @@ TEST(Database, DisplayDatabase) {
     Database db;
     db.add(abacki);
 
-    const std::string ref_db_display = "first_name|last_name|street|apartment|postal_code|city|index_num|pesel|gender\nAdam|Abacki|Warszawska 42|2|01-234|Warszawa|1|55030101193|male\n";
+    const std::string ref_db_display =
+        "first_name|last_name|street|apartment|postal_code|city|index_num|pesel|gender\n"
+        "Adam|Abacki|Warszawska 42|2|01-234|Warszawa|1|55030101193|male\n";
 
     EXPECT_EQ(db.display(), ref_db_display);
 }
