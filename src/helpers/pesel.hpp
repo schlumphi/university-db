874f644912be4f016b4b5cfc989c8dd76ce5fe6f
schlumphi <j.zarzycki777@gmail.com>
2026-03-16 10:40:04 +0100

    feat: datime from pesel

--- src/helpers/pesel.hpp
+++ src/helpers/pesel.hpp
@@ -6,12 +6,21 @@
 #include <string_view>
 #include "gender.hpp"
 
+enum class Century {
+    Nineteenth,
+    Twentieth,
+    TwentyFirst,
+    TwentySecond,
+    TwentyThird
+};
+
 class Pesel {
 public:
     enum class ErrorCode {
         InvalidPeselFormat,
         InvalidPeselChecksum
     };
+
     Pesel(const std::string& number);
 
     auto value() const noexcept -> const std::string& { return m_number; }
@@ -23,12 +32,20 @@ public:
     static constexpr std::array<uint8_t, 11> weights{1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
 
     // checks if lhs is younger than rhs
-    auto operator<(const Pesel& rhs) const noexcept -> bool {
-        return false;
-    }
+    auto operator<(const Pesel& rhs) const noexcept -> bool;
 
 private:
     const std::string m_number;
 };
 
 auto parse_pesel_error_code(Pesel::ErrorCode error) -> std::string_view;
+
+auto derive_century(const Pesel& pesel) -> Century;
+
+auto parse_day(const Pesel& pesel) -> uint64_t;
+
+auto parse_month(const Pesel& pesel, const Century century) -> uint64_t;
+
+auto parse_year(const Pesel& pesel, const Century century) -> uint64_t;
+
+auto approx_days_since_epoch(const Pesel& pesel) -> uint64_t;
