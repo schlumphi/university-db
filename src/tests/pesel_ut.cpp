c3be2d78be03e80d1e59ac463561febdbcaebf12
schlumphi <j.zarzycki777@gmail.com>
2026-03-22 09:04:26 +0100

    test: tests without error codes comparision

--- src/tests/pesel_ut.cpp
+++ src/tests/pesel_ut.cpp
@@ -3,15 +3,12 @@
 #include "helpers/pesel.hpp"
 
 TEST(Pesel, ConstructorWithInvalidValidFormatThrowsError) {
-    EXPECT_EQ(Pesel::validate_format("9876543210"), Pesel::ErrorCode::InvalidPeselFormat);
     EXPECT_THROW(Pesel("9876543210"), std::invalid_argument);
 
-    EXPECT_EQ(Pesel::validate_format("9876543210c"), Pesel::ErrorCode::InvalidPeselFormat);
     EXPECT_THROW(Pesel("9876543210c"), std::invalid_argument);
 }
 
 TEST(Pesel, ConstructorWithInvalidChecksumThrowsError) {
-    EXPECT_EQ(Pesel::validate_correctness("98765432100"), Pesel::ErrorCode::InvalidPeselChecksum);
     EXPECT_THROW(Pesel("98765432100"), std::invalid_argument);
 }
 
