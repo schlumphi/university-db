76127cced6090f2265c26d0cf3d25a48ff509794
schlumphi <j.zarzycki777@gmail.com>
2026-03-22 11:16:31 +0100

    test: better readability with fixture

--- src/tests/address_ut.cpp
+++ src/tests/address_ut.cpp
@@ -3,6 +3,14 @@
 #include "helpers/address.hpp"
 #include "helpers/postal_code.hpp"
 
+class AddressTest : public ::testing::Test {
+protected:
+    const std::string valid_street{"Sezamkowa 42"};
+    const std::string valid_apartment{"2"};
+    const PostalCode valid_postal_code{"01-234"};
+    const std::string valid_city{"Warsaw"};
+};
+
 TEST(Address, ConstructorWithValidDataCreatesValidObject) {
     EXPECT_NO_THROW(Address("Sezamkowa 42", "2", PostalCode{"01-234"}, "Warsaw"));
     EXPECT_NO_THROW(Address("Sezamkowa 42", "2", PostalCode{"01-234"}, "New York"));
@@ -13,100 +21,88 @@ TEST(Address, ConstructorWithValidDataCreatesValidObject) {
     EXPECT_NO_THROW(Address("Edwarda Rydza-Smiglego 42", "2", PostalCode{"01-234"}, "Warsaw"));
 }
 
-TEST(Address, ConstructorWithInvalidStreetNameThrowsError) {
-    const auto apartment = std::string{""};
-    const auto postal_code = PostalCode{"01-234"};
-    const auto city = std::string{"Warsaw"};
-
+TEST_F(AddressTest, ConstructorWithInvalidStreetNameThrowsError) {
     EXPECT_THROW(
-        Address("", apartment, postal_code, city),
+        Address("", valid_apartment, valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address("Warszawska", apartment, postal_code, city),
+        Address("Warszawska", valid_apartment, valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address("Wars#zawska 2", apartment, postal_code, city),
+        Address("Wars#zawska 2", valid_apartment, valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address("Warszawska F", apartment, postal_code, city),
+        Address("Warszawska F", valid_apartment, valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address("Warszawska F2", apartment, postal_code, city),
+        Address("Warszawska F2", valid_apartment, valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address("Warszawska 2F2", apartment, postal_code, city),
+        Address("Warszawska 2F2", valid_apartment, valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address("11 Listopada", apartment, postal_code, city),
+        Address("11 Listopada", valid_apartment, valid_postal_code, valid_city),
         std::invalid_argument);
 
     // EXPECT_THROW(
-    //     Address("!@#$ 2", apartment, postal_code, city),
+    //     Address("!@#$ 2", valid_apartment, valid_postal_code, valid_city),
     //     std::invalid_argument);
 
     // EXPECT_THROW(
-    //     Address("!@#$", apartment, postal_code, city),
+    //     Address("!@#$",, valid_apartment, valid_postal_code, valid_city),
     //     std::invalid_argument);
 }
 
-TEST(Address, ConstructorWithInvalidApartmentThrowsError) {
-    const auto street = std::string{"Sezamkowa"};
-    const auto postal_code = PostalCode{"01-234"};
-    const auto city = std::string{"Warsaw"};
-
+TEST_F(AddressTest, ConstructorWithInvalidApartmentThrowsError) {
     EXPECT_THROW(
-        Address(street, "/", postal_code, city),
+        Address(valid_street, "/", valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, "2/", postal_code, city),
+        Address(valid_street, "2/", valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, "/2", postal_code, city),
+        Address(valid_street, "/2", valid_postal_code, valid_city),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, "2F#", postal_code, city),
+        Address(valid_street, "2F#", valid_postal_code, valid_city),
         std::invalid_argument);
 }
 
-TEST(Address, ConstructorWithInvalidCityNameThrowsError) {
-    const auto street = std::string{"Sezamkowa"};
-    const auto apartment = std::string{"42"};
-    const auto postal_code = PostalCode{"01-234"};
-
+TEST_F(AddressTest, ConstructorWithInvalidCityNameThrowsError) {
     EXPECT_THROW(
-        Address(street, apartment, postal_code, ""),
+        Address(valid_street, valid_apartment, valid_postal_code, ""),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, apartment, postal_code, "A"),
+        Address(valid_street, valid_apartment, valid_postal_code, "A"),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, apartment, postal_code, "a"),
+        Address(valid_street, valid_apartment, valid_postal_code, "a"),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, apartment, postal_code, "a42"),
+        Address(valid_street, valid_apartment, valid_postal_code, "a42"),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, apartment, postal_code, "A42"),
+        Address(valid_street, valid_apartment, valid_postal_code, "A42"),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, apartment, postal_code, "oS"),
+        Address(valid_street, valid_apartment, valid_postal_code, "oS"),
         std::invalid_argument);
 
     EXPECT_THROW(
-        Address(street, apartment, postal_code, "OS"),
+        Address(valid_street, valid_apartment, valid_postal_code, "OS"),
         std::invalid_argument);
 }
