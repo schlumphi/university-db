fc16eda685aa8310ee8f0d38ff869436d103bdf2
schlumphi <j.zarzycki777@gmail.com>
2026-04-01 21:16:02 +0200

    refactor: student derived from person

--- src/core/student.hpp
+++ src/core/student.hpp
@@ -6,34 +6,20 @@
 #include "helpers/address.hpp"
 #include "helpers/gender.hpp"
 #include "helpers/pesel.hpp"
+#include "person.hpp"
 
 class Database;
 
-class Student {
+class Student : public Person {
 public:
     Student(
         const std::string& first_name,
         const std::string& last_name,
         const Address& address,
         const Pesel& pesel,
-        const Gender gender);
+        const Gender gender) : Person(first_name, last_name, address, pesel, gender), m_index_num(0ULL) {}
 
-    bool operator==(const Student& other) const noexcept {
-        if (other.pesel().value() == m_pesel.value()) {
-            return true;
-        }
-        return false;
-    }
-
-    const std::string& first_name() const noexcept { return m_first_name; }
-    const std::string& last_name() const noexcept { return m_last_name; }
-    const Address& address() const noexcept { return m_address; }
-    const Pesel& pesel() const noexcept { return m_pesel; }
     uint64_t index_num() const noexcept { return m_index_num; }
-    Gender gender() const noexcept { return m_gender; }
-
-    static std::string validate_name(const std::string& name);
-    static Gender validate_gender(const Pesel& pesel, const Gender gender);
 
 private:
     void set_index_num(const uint64_t index_num) {
@@ -42,12 +28,7 @@ private:
         }
     }
 
-    const std::string m_first_name;
-    const std::string m_last_name;
-    const Address m_address;
-    const Pesel m_pesel;
     uint64_t m_index_num;
-    const Gender m_gender;
 
     friend class Database;
 };
