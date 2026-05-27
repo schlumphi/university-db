216b1b1642e03a36b4a05ffcb5479084c3205ba2
schlumphi <j.zarzycki777@gmail.com>
2026-05-27 19:32:40 +0200

    feat: assign starting salary method

--- src/core/database.hpp
+++ src/core/database.hpp
@@ -43,6 +43,7 @@ public:
 
 private:
     Person* find_by_pesel(const Pesel& pesel) noexcept;
+    void assign_starting_salary(Employee& employee);
     // void validate_loaded_header(const std::string& header, const char sep = '|') const;
     // bool is_index_taken(const uint64_t index_num) const noexcept;
     // uint64_t find_highest_index_num() const noexcept;
